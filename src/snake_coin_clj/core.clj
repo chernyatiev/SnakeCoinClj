669afa8ef157a41c868f918ccd23c4d2748b2420
Alexander Chernyatiev <chernyatiev@gmail.com>
2017-09-22 17:37:11 +0300

    add digest lib

--- src/snake_coin_clj/core.clj
+++ src/snake_coin_clj/core.clj
@@ -1,6 +1,34 @@
-(ns snake-coin-clj.core)
+(ns snake-coin-clj.core
+  (:require [digest]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def blockchain (atom []))
+
+
+(defn hash-block [index timestamp data previous-hash]
+  (let [s (str index timestamp data previous-hash)]
+    (digest/sha-256 s)))
+
+(defn create-block
+  "create hash-map with block data"
+  [index timestamp data previous-hash]
+
+  {:index index
+   :timestamp timestamp
+   :data data
+   :previous-hash previous-hash
+   :hash (hash-block index timestamp data previous-hash)})
+
+
+(defn get-last-block []
+  (last @blockchain))
+
+(defn create-genesis-block [])
+
+
+(defn add-block-to-blockchain [block]
+  (swap! blockchain ))
+
+
+(defn create-next-block [data]
+  (let [previous-block (get-last-block)]))
